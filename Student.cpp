1429f2647cd56eca29a519bd661b0d3a572c2c57
IT21127946 <86715390+HilarinaChristy@users.noreply.github.com>
2021-08-18 12:34:45 +0000

    Implemented the methods

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,14 @@
 #include "Student.h"
 #include <iostream>
-
+#include<cstring>
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int sId,char sname[]) {
+  studentID =sId;
+  strcpy(name,sname);
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display() {
+  cout<<"Student ID:"<<studentId<<endl;
+  cout<<"Student Name:"<<name<<endl;
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,17 @@
 class Student {
-  // private section
+  // rivate section
   //    int studentId
   //    name <- 20 charcters
   
   // public section
   //    assignDetails() method declaration
   //    display() method declaration
+
+  private:
+   int studentId;
+   char name[20];
+
+   public:
+    void assignDetails(int sId,char sname[]);
+    void display();
 };
